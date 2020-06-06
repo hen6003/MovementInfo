9ac9e3848cd91b08452753cd9a629f0a28aa87ae
hen6003 <henrysmith6003@protonmail.com>
2020-06-06 23:34:10 +0100

    added support for 1.16

--- src/main/java/com/hen6003/mi/mixin/GameRendererMixin.java
+++ src/main/java/com/hen6003/mi/mixin/GameRendererMixin.java
@@ -8,20 +8,23 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.Window;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3f;
+//import net.minecraft.entity.boss.BossBar;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Util;
-//import net.minecraft.block.IceBlock;
 import net.minecraft.util.math.BlockPos;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
 
-	@Inject(at = @At(value = "INVOKE", target = "com/mojang/blaze3d/systems/RenderSystem.defaultAlphaFunc()V"), method = "render")
-	public void render(float float_1, long long_1, boolean boolean_1, CallbackInfo info) {
+	@Inject(at = @At(value = "INVOKE", target = "com/mojang/blaze3d/systems/RenderSystem.defaultAlphaFunc()V"), method = "render", locals = LocalCapture.CAPTURE_FAILSOFT)
+	public void render(float float_1, long long_1, boolean boolean_1, CallbackInfo info , int i, int j, Window window, MatrixStack matrixStack) {
 
 		MinecraftClient client = MinecraftClient.getInstance();
 		PlayerEntity playerEntity = (PlayerEntity) client.player;
@@ -43,9 +46,9 @@ public class GameRendererMixin {
 			}
 
 			if (playerEntity.getVehicle() != null){
-			miString += "[" + playerEntity.getVehicle().getDisplayName().asFormattedString() + "]";
+			miString += "[" + playerEntity.getVehicle().getDisplayName().asString()+ "]";
 				
-				if (!playerEntity.getVehicle().onGround & !playerEntity.getVehicle().isSubmergedInWater()){
+				if (!playerEntity.getVehicle().isOnGround() & !playerEntity.getVehicle().isSubmergedInWater()){
 				miString += "[Jumping]";
 				}
 			} else if (playerEntity.abilities.flying){
@@ -54,7 +57,7 @@ public class GameRendererMixin {
 				miString += "[Flying]";
 			} else if (playerEntity.isClimbing()){
 				miString += "[Climbing]";
-			} else if (!playerEntity.onGround & !playerEntity.isSwimming()) {
+			} else if (!playerEntity.isOnGround() & !playerEntity.isSwimming()) {
 				miString += "[Jumping]";
 			}
 
@@ -96,7 +99,6 @@ public class GameRendererMixin {
 			int playerBPS = (int)(Math.sqrt(tempVector.dot(tempVector)));
 
 			float slipperiness = playerEntity.world.getBlockState(new BlockPos(MIMod.newBlockPos.getX(), MIMod.newBlockPos.getY() - 1f, MIMod.newBlockPos.getZ())).getBlock().getSlipperiness();
-			System.out.println(slipperiness);
 			if (slipperiness > 0.6f){
 				miString += "[Sliding]";
 			}
@@ -111,23 +113,25 @@ public class GameRendererMixin {
 				psString += "[CPS:" + MIMod.cps + "]";
 			}
 
+			//BossBar
+
 			float textPosX = 5;
 			float bpsTextPosX = 5;
 
 			if (MICommands.config.align == 1) {
-				textPosX = (client.getWindow().getScaledWidth() - client.textRenderer.getStringWidth(miString)) / 2f;
-				bpsTextPosX = (client.getWindow().getScaledWidth() - client.textRenderer.getStringWidth(psString)) / 2f;
+				textPosX = (client.getWindow().getScaledWidth() - client.textRenderer.getWidth(miString)) / 2f;
+				bpsTextPosX = (client.getWindow().getScaledWidth() - client.textRenderer.getWidth(psString)) / 2f;
 			}
 			if (MICommands.config.align == 2) {
-				textPosX = client.getWindow().getScaledWidth() - client.textRenderer.getStringWidth(miString) - textPosX;
-				bpsTextPosX = client.getWindow().getScaledWidth() - client.textRenderer.getStringWidth(psString) - bpsTextPosX;
+				textPosX = client.getWindow().getScaledWidth() - client.textRenderer.getWidth(miString) - textPosX;
+				bpsTextPosX = client.getWindow().getScaledWidth() - client.textRenderer.getWidth(psString) - bpsTextPosX;
 			}
 
 			if (!MICommands.config.onlyPs){
-				client.textRenderer.drawWithShadow(miString, textPosX, 5, MICommands.config.hudColor);
-				client.textRenderer.drawWithShadow(psString, bpsTextPosX, 15, MICommands.config.hudColor);
+				client.textRenderer.drawWithShadow(matrixStack, miString, textPosX, 5, MICommands.config.hudColor);
+				client.textRenderer.drawWithShadow(matrixStack, psString, bpsTextPosX, 15, MICommands.config.hudColor);
 			} else {
-				client.textRenderer.drawWithShadow(psString, bpsTextPosX, 5, MICommands.config.hudColor);
+				client.textRenderer.drawWithShadow(matrixStack, psString, bpsTextPosX, 5, MICommands.config.hudColor);
 			}
 
 			RenderSystem.popMatrix();
